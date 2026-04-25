3b2292987483e032b5c8c705ba8f7b159708c0ee
Arnoldnl23 <arnoldnl@miamioh.edu>
2026-04-25 12:15:54 -0400

    Implemented level select menu
    Still needs tested in VR
    Unlocking levels should work

---
+++ Ricochet Puzzle/Assets/Ricochet Game/Scripts/LevelSelector.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelector : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject levelMenu;
+    private float distance = 2.0f;
+
+    public Button[] buttons;
+
+    public void createLevelMenu()
+    {
+        GameObject levelUI = Instantiate(levelMenu);
+        levelMenu.transform.position = player.transform.position + (player.transform.forward * distance);
+        levelMenu.transform.rotation = player.transform.rotation;
+
+        buttons = new Button[10];
+        for (int i = 0; i < 10; i++)
+        {
+            buttons[i] = levelMenu.transform.GetChild(i).GetComponent<Button>();
+        }
+
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    public void openLevel(int levelNum)
+    {
+        SceneManager.LoadScene(levelNum);
+    }
+
+    public void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+        }
+    }
+}
