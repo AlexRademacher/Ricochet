46d25a302f5d2a278eb65683a95cff7f65b436c5
AlexRademacher <178829078+AlexRademacher@users.noreply.github.com>
2026-04-29 13:06:26 -0400

    Gun work
    Features:
    - New Ball model
    
    Edits:
    - Gun script is now more readable with comments
    - Allowed variables to be messed with in unity
    - Put in checks for edge cases and nulls
    
    Issues
    - Bullet instantiate does not like spline animate
    - a line for the laser line is out of bounds and is currently commented out

--- Ricochet Puzzle/Assets/Ricochet Game/Scripts/GunScript.cs
+++ Ricochet Puzzle/Assets/Ricochet Game/Scripts/GunScript.cs
@@ -1,19 +1,20 @@
 
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Splines;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
 public class GunScript : MonoBehaviour
 {
-    public Transform firePoint;
+    [Header("Gun Stats")]
+    [SerializeField]
+    private Transform firePoint;
+    [Range(0, 50), SerializeField]
     private float range = 50f;
 
     private float gunMoveRange = 1f;
 
+    [Range(0, 5), SerializeField]
     private int maxBounce = 3;
     private int currBounce = 0;
 
@@ -21,15 +22,27 @@ public class GunScript : MonoBehaviour
 
     private bool isFiring = false;
 
-    private int currShot = 0;
+    [Range(0, 10), SerializeField]
     private int maxShot = 3;
+    private int currShot = 0;
 
     private Vector3 startPos;
+    private Quaternion startRot;
 
     private XRBaseInteractable interactable;
-    public GameObject bulletPrefab;
+    [Header("Other Prefabs")]
+    [SerializeField]
+    private GameObject bulletPrefab;
     private SplineContainer splinePath;
     private SplineAnimate splineAnim;
+
+    [SerializeField]
+    private GameObject failureUI;
+    [SerializeField]
+    private GameObject successUI;
+    [SerializeField]
+    private GameObject player;
+    private float distance = 1.0f;
 
     void Start()
     {
@@ -37,6 +50,7 @@ public class GunScript : MonoBehaviour
         interactable = GetComponent<XRBaseInteractable>();
 
         startPos = transform.position;
+        startRot = transform.rotation;
     }
 
     void Update()
@@ -44,6 +58,7 @@ public class GunScript : MonoBehaviour
         if (Mathf.Abs(transform.position.x - startPos.x) > gunMoveRange || Mathf.Abs(transform.position.z - startPos.z) > gunMoveRange) {
             interactable.enabled = false;
             transform.position = startPos;
+            transform.rotation = startRot;
             interactable.enabled = true;
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
@@ -52,99 +67,158 @@ public class GunScript : MonoBehaviour
         if (!isFiring) {
             RayCast(new Ray(firePoint.position, firePoint.forward));
         }
+
+        if (!isFiring && Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
     }
 
+    /// <summary>
+    /// Creates an Aiming Line for shooting
+    /// </summary>
+    /// <param name="ray"> The Ray that is used for aiming the bullets</param>
     private void RayCast(Ray ray)
     {
-        RaycastHit hit;
 
         laserLine.positionCount = 2;
         laserLine.SetPosition(0, firePoint.position);
 
-        if (Physics.Raycast(firePoint.position, -1 * firePoint.forward, out hit, range))
+        if (Physics.Raycast(firePoint.position, firePoint.forward, out RaycastHit hit, range))
         {
             laserLine.SetPosition(1, hit.point);
         }
         else
         {
-            laserLine.SetPosition(1, firePoint.position + (-1  * firePoint.forward * range));
+            laserLine.SetPosition(1, firePoint.position + (firePoint.forward * range));
         }
     }
 
-    IEnumerator multiRayCast(Ray ray)
+    /// <summary>
+    /// Ricochets the Shooting Line off the Walls till the <c>maxBounce</c>
+    /// </summary>
+    /// <param name="ray">The Line that we are shooting along</param>
+    /// <returns></returns>
+    IEnumerator MultiRayCast(Ray ray)
     {
+        // remove firing again
         isFiring = true;
-        RaycastHit hit;
+
+        // hides Laser Line
         laserLine.enabled = false;
-        if (Physics.Raycast(ray, out hit, range) && currBounce <= maxBounce)
+
+        // Uses recursion hitting things and ricocheting back till the maxBounce is reached
+        if (Physics.Raycast(ray, out RaycastHit hit, range) && currBounce <= maxBounce)
         {
-            currBounce++;
+            // Updates bounce num
+            currBounce++; 
+
+            // Get new angle
             var reflectAngle = Vector3.Reflect(ray.direction, hit.normal);
+
+            // Update Laser Line
             laserLine.positionCount = currBounce + 1;
             laserLine.SetPosition(currBounce, hit.point);
-            StartCoroutine(multiRayCast(new Ray(hit.point, reflectAngle)));
+
+            // Loops
+            StartCoroutine(MultiRayCast(new Ray(hit.point, reflectAngle)));
         }
         else
         {
+            // updates Laser Line
             laserLine.positionCount = currBounce + 1;
-            laserLine.SetPosition(currBounce + 2, new Vector3(laserLine.GetPosition(currBounce - 1).x, laserLine.GetPosition(currBounce - 1).y, laserLine.GetPosition(currBounce - 1).z));
+            //laserLine.SetPosition(currBounce + 1, new Vector3(laserLine.GetPosition(currBounce - 1).x, laserLine.GetPosition(currBounce - 1).y, laserLine.GetPosition(currBounce - 1).z));
+            // Is out of bounds with the laser line
         }
         yield return new WaitForSeconds(0);
     }
 
+    /// <summary>
+    /// Called to shoot the gun
+    /// </summary>
     public void Fire()
     {
-        RaycastHit hit;
-
-        if (Physics.Raycast(firePoint.position, -1 * firePoint.forward, out hit, range) && !isFiring)
+        // Makes a ray that points forward if not shooting already
+        if (Physics.Raycast(firePoint.position, firePoint.forward, out RaycastHit hit, range) && !isFiring)
         {
+            // set bounce cound to 0
             currBounce = 0;
+
+            // Sets up Laser Line
             laserLine.positionCount = 1;
             laserLine.SetPosition(0, firePoint.position);
-            StartCoroutine(multiRayCast(new Ray(firePoint.position, -1 * firePoint.forward)));
 
+            // Shoots the RayCast out and has it bounce on all of the walls with recursion
+            StartCoroutine(MultiRayCast(new Ray(firePoint.position, firePoint.forward)));
+
+            // Setting up spline
             Time.timeScale = 0;
-            GameObject bullet = Instantiate(bulletPrefab, new Vector3(firePoint.position.x, firePoint.position.y, firePoint.position.z), firePoint.rotation);
-            splinePath = bullet.GetComponent<SplineContainer>();
-            splineAnim = bullet.GetComponent<SplineAnimate>();
 
-            Spline spline = splinePath.Spline;
-            spline.Clear();
-            for (int i = 0; i < laserLine.positionCount; i++)
+            if (bulletPrefab != null)
             {
-                spline.Add(new BezierKnot(laserLine.GetPosition(i)));           
+                GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); // <--- Transform.postion doesn't work correctly with spline Animate look into setting spline animate in code after instantiate
+                splinePath = bullet.GetComponent<SplineContainer>();
+                splineAnim = bullet.transform.GetChild(0).GetComponent<SplineAnimate>();
+
+                if (splinePath != null)
+                {
+                    Spline spline = splinePath.Spline;
+                    spline.Clear();
+
+                    for (int i = 0; i < laserLine.positionCount; i++)
+                    {
+                        spline.Add(new BezierKnot(laserLine.GetPosition(i)));
+                    }
+
+                    spline.Closed = false;
+                    Time.timeScale = 1;
+
+                    StartCoroutine(AfterShoot(bullet));
+                }
+                else
+                    Debug.LogError("Spline Path is missing");
             }
-            spline.Closed = false;
-            Time.timeScale = 1;
-            
-            StartCoroutine(afterShoot(bullet));
+            else
+                Debug.LogError("Bullet Prefab is missing");
         }
     }
 
-    public GameObject failureUI;
-    public GameObject successUI;
-    public GameObject player;
-    private float distance = 1.0f;
 
-    IEnumerator afterShoot(GameObject bullet)
+    IEnumerator AfterShoot(GameObject bullet)
     {
-        splineAnim.Play();
-        yield return new WaitUntil(() => splineAnim.IsPlaying == false);
+        // Starts spline
+        if (splineAnim != null)
+        {
+            splineAnim.Play();
+            yield return new WaitUntil(() => splineAnim.IsPlaying == false);
+        }
+        else
+            Debug.LogError("Spline Animate is missing");
 
+        // Removes bullet
         Destroy(bullet);
         currShot++;
 
+        // Checks if player lost, hit all targets, or continues shooting
         if (currShot > maxShot)
         {
-            GameObject failUI = Instantiate(failureUI);
-            failUI.transform.position = player.transform.position + (player.transform.forward * distance) - new Vector3(0,0,0.5f);
-            failUI.transform.rotation = player.transform.rotation;
+            if (failureUI != null)
+            {
+                GameObject failUI = Instantiate(failureUI);
+                failUI.transform.SetPositionAndRotation(player.transform.position + (player.transform.forward * distance) - new Vector3(0, 0, 0.5f), player.transform.rotation);
+            }
+            else
+                Debug.LogError("Failure UI is missing");
         }
         else if (GameObject.FindGameObjectsWithTag("Target").Length == 0)
         {
+            if (successUI)
+            {
                 GameObject winUI = Instantiate(successUI);
-                winUI.transform.position = player.transform.position + (player.transform.forward * distance) - new Vector3(0, 0, 0.5f);
-                winUI.transform.rotation = player.transform.rotation;
+                winUI.transform.SetPositionAndRotation(player.transform.position + (player.transform.forward * distance) - new Vector3(0, 0, 0.5f), player.transform.rotation);
+            }
+            else
+                Debug.LogError("Success UI is missing");
         }
         else
         {
